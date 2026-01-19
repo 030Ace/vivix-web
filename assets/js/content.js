afe98166667685391912c466cc2533b678c00a22
030Ace <arselys@gmail.com>
2026-01-20 00:57:42 +0100

    Create Website
    Website erstellung v1

---
+++ assets/js/content.js
@@ -0,0 +1,75 @@
+// Konfiguration für Clients und Partner Logos
+// Hier kannst du die Bildpfade und die Ziel-URLs anpassen.
+// Wenn ein Link leer ist (""), passiert beim Klicken nichts (oder du kannst '#' nutzen).
+
+const clientData = [
+  { src: "assets/images/clients/BMW logo.png", link: "https://www.bmw.com" },
+  { src: "assets/images/clients/arosalenzerheide logo.png", link: "https://arosalenzerheide.swiss/de/Skigebiet" },
+  { src: "assets/images/clients/bentley logo.png", link: "https://www.bentleymotors.com/" },
+  { src: "assets/images/clients/blacklisted logo.png", link: "https://www.instagram.com/blacklistedglobal/" },
+  { src: "assets/images/clients/como logo.png", link: "https://www.comoberlin.de/" },
+  { src: "assets/images/clients/corner 33 logo.png", link: "https://corner33.nl/" },
+  { src: "assets/images/clients/evan luthra.png", link: "https://www.instagram.com/evanluthra/" },
+  { src: "assets/images/clients/goldjunge logo.png", link: "https://goldjunge.simplybook.it/v2/" },
+  { src: "assets/images/clients/ivangette logo.png", link: "https://ivangette.com/" },
+  { src: "assets/images/clients/kolcapital logo.png", link: "https://kolcapital.io/" },
+  { src: "assets/images/clients/luciano logo.png", link: "https://www.instagram.com/lucianoloco/" },
+  { src: "assets/images/clients/notagallery logo.png", link: "https://notagallery.de/" },
+  { src: "assets/images/clients/opendesci logo.png", link: "https://www.opendesci.org/" },
+  { src: "assets/images/clients/sapphire logo.png", link: "https://sapphiresuitcases.com/" },
+  { src: "assets/images/clients/the door logo.png", link: "https://thedoor.club" },
+  { src: "assets/images/clients/void logo.png", link: "https://www.instagram.com/voidmykonos/" },
+  { src: "assets/images/clients/waldorf astoria logo.png", link: "https://waldorfastoriaberlin.de/" }
+];
+
+const partnerData = [
+  { src: "assets/images/partners/aparo logo.png", link: "https://www.aparolight.com/" },
+  { src: "assets/images/partners/digitalfoto logo.png", link: "https://www.instagram.com/dfdigitalfoto/" },
+  { src: "assets/images/partners/eirmai logo.png", link: "https://www.eirmai.com/en/" },
+  { src: "assets/images/partners/hollyland logo.png", link: "https://www.hollyland.com/de" },
+  { src: "assets/images/partners/lexar logo.png", link: "https://www.lexar.com/de/" },
+  { src: "assets/images/partners/moft logo.png", link: "https://moftde.com/" },
+  { src: "assets/images/partners/musso logo.png", link: "https://musso.eu/de" },
+  { src: "assets/images/partners/neewer logo.png", link: "https://www.instagram.com/neewer/" },
+  { src: "assets/images/partners/patona logo.png", link: "https://www.instagram.com/patona_de/" },
+  { src: "assets/images/partners/pgy tech logo.png", link: "https://www.pgytech.com/de?srsltid=AfmBOorP0GjxYQPUNAaQgsLSSBchlSYIlFf2kKu6BBrCW7WIthpYuiUj" },
+  { src: "assets/images/partners/plaud ai logo.png", link: "https://www.instagram.com/plaud_official/" },
+  { src: "assets/images/partners/raubay logo.png", link: "https://www.instagram.com/raubayofficial/" },
+  { src: "assets/images/partners/sharge logo.png", link: "https://www.instagram.com/sharge_official/" },
+  { src: "assets/images/partners/torras logo.png", link: "https://www.instagram.com/torras_global/" },
+  { src: "assets/images/partners/verbatim logo.png", link: "https://www.verbatim-europe.com/de" }
+];
+
+// Gallery Menu Images
+const menuImages = [
+  { src: "assets/images/hamburger_menu/Eirmai .jpg", link: "https://www.eirmai.com/en/" },
+  { src: "assets/images/hamburger_menu/Ivan Gette Koffer 1.jpg", link: "https://www.instagram.com/ivangette_art/" },
+  { src: "assets/images/hamburger_menu/Ivan Gette Koffer 2.jpg", link: "https://www.instagram.com/ivangette_art/" },
+  { src: "assets/images/hamburger_menu/Ivan Gette Skateboards 1jpg.jpg", link: "https://www.instagram.com/ivangette_art/" },
+  { src: "assets/images/hamburger_menu/Ivan Gette Skateboards 2.jpg", link: "https://www.instagram.com/ivangette_art/" },
+  { src: "assets/images/hamburger_menu/Sapphire Suitcases 2.jpg", link: "https://sapphiresuitcases.com/" },
+  { src: "assets/images/hamburger_menu/Sapphire suitcases 1.jpg", link: "https://sapphiresuitcases.com/" },
+  { src: "assets/images/hamburger_menu/blacklisted 1.jpg", link: "https://www.instagram.com/blacklistedglobal/" },
+  { src: "assets/images/hamburger_menu/blacklisted 2.jpg", link: "https://www.instagram.com/blacklistedglobal/" },
+  { src: "assets/images/hamburger_menu/bmw 1.jpg", link: "https://www.bmw.com" },
+  { src: "assets/images/hamburger_menu/bmw 2.jpg", link: "https://www.bmw.com" },
+  { src: "assets/images/hamburger_menu/bmw 3.jpg", link: "https://www.bmw.com" },
+  { src: "assets/images/hamburger_menu/bmw 4.jpg", link: "https://www.bmw.com" },
+  { src: "assets/images/hamburger_menu/bmw 5.jpg", link: "https://www.bmw.com" },
+  { src: "assets/images/hamburger_menu/bmw 6.jpg", link: "https://www.bmw.com" },
+  { src: "assets/images/hamburger_menu/como 1.jpg", link: "https://www.comoberlin.de/" },
+  { src: "assets/images/hamburger_menu/como 2.jpg", link: "https://www.comoberlin.de/" },
+  { src: "assets/images/hamburger_menu/como 3.jpg", link: "https://www.comoberlin.de/" },
+  { src: "assets/images/hamburger_menu/como 4.jpg", link: "https://www.comoberlin.de/" },
+  { src: "assets/images/hamburger_menu/como 5.jpg", link: "https://www.comoberlin.de/" },
+  { src: "assets/images/hamburger_menu/como 6.png", link: "https://www.comoberlin.de/" },
+  { src: "assets/images/hamburger_menu/corner 33.jpg", link: "https://corner33.nl/" },
+  { src: "assets/images/hamburger_menu/dubai.jpg", link: "#" },
+  { src: "assets/images/hamburger_menu/jaegermeister.jpg", link: "#" },
+  { src: "assets/images/hamburger_menu/sapphire suitcases 3.jpg", link: "https://sapphiresuitcases.com/" },
+  { src: "assets/images/hamburger_menu/sharge 1.jpg", link: "https://www.instagram.com/sharge_official/" },
+  { src: "assets/images/hamburger_menu/sharge 2.jpg", link: "https://www.instagram.com/sharge_official/" },
+  { src: "assets/images/hamburger_menu/torras.jpg", link: "https://www.instagram.com/torras_global/" },
+  { src: "assets/images/hamburger_menu/ushuaya.jpg", link: "#" },
+  { src: "assets/images/hamburger_menu/void mykonos 1.jpg", link: "https://www.instagram.com/voidmykonos/" }
+];
